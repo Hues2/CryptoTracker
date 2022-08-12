167ed9bfa4474febe42d1a97b7536f0e47785e67
Greg Ross <gregandrewross79@gmail.com>
2022-08-12 22:45:29 +0100

    - Refactored code and made the networking code more reusable with the networking manager class - End of #7

--- SwiftfulCrypto/Services/CoinDataService.swift
+++ SwiftfulCrypto/Services/CoinDataService.swift
@@ -12,7 +12,7 @@ class CoinDataService : ObservableObject{
     @Published var allCoins = [CoinModel]()
     @Published var portfolioCoins = [CoinModel]()
     
-    private var cancellable: AnyCancellable?
+    private var coinSubscription: AnyCancellable?
     
     init(){
         getCoins()
@@ -22,27 +22,13 @@ class CoinDataService : ObservableObject{
     private func getCoins(){
         guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h") else {return}
         
-        cancellable = URLSession.shared.dataTaskPublisher(for: url)
-            .subscribe(on: DispatchQueue.global(qos: .background))
-            .receive(on: DispatchQueue.main)
-            .tryMap { (output) -> Data in
-                guard let response = output.response as? HTTPURLResponse,
-                      response.statusCode >= 200 && response.statusCode < 300 else{
-                    throw URLError(.badServerResponse)
-                      }
-                return output.data
-            }
+        coinSubscription = NetworkingManager.download(url: url)
             .decode(type: [CoinModel].self, decoder: JSONDecoder())
             .sink { completion in
-                switch completion{
-                case .finished:
-                    break
-                case .failure(let error):
-                    print("\n \(error.localizedDescription) \n")
-                }
+                NetworkingManager.handleCompletion(completion: completion)
             } receiveValue: { [weak self] returnedCoins in
                 self?.allCoins = returnedCoins
-                self?.cancellable?.cancel()
+                self?.coinSubscription?.cancel()
             }
 
     }
