39c799cf3375dbb56357a7ffdfa48f7c182dd80b
Greg Ross <gregandrewross79@gmail.com>
2022-08-15 19:42:12 +0100

    - Added refreshable functionality for coin lists - Added UINotificationFeedbackGenerator for haptics - Added functionality that calculates portfolio value - End of #16

--- SwiftfulCrypto/Core/Home/ViewModels/HomeViewModel.swift
+++ SwiftfulCrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -20,6 +20,8 @@ class HomeViewModel: ObservableObject{
     @Published var allCoins: [CoinModel] = []
     @Published var portfolioCoins: [CoinModel] = []
     
+    @Published var isLoading: Bool = false
+    
     @Published var searchText: String = ""
     
     private let coinDataService = CoinDataService()
@@ -51,49 +53,41 @@ class HomeViewModel: ObservableObject{
             .store(in: &cancellables)
         
         
+        
+        // Updates portfolioCoins
+        $allCoins
+            .combineLatest(portfolioDataService.$savedEntities)
+//            .map({ (coinModels, portfolioEntities) -> [CoinModel] in
+//                coinModels.compactMap { (coin) -> CoinModel? in
+//                    guard let entity = portfolioEntities.first(where: {$0.coinId == coin.id}) else {return nil}
+//
+//                    return coin.updateHoldings(amount: entity.amount)
+//
+//                }
+//            })
+            .map(mapAllCoinsToPortfolioCoins)
+            .sink { [weak self] returnedPortfolioCoins in
+                self?.portfolioCoins = returnedPortfolioCoins
+            }
+            .store(in: &cancellables)
+        
+        
+        
         // Updates marketData
         marketDataService.$marketData
+            .combineLatest($portfolioCoins)
             // This publisher returns MarketDataModel, but we need to create a list of statistics
             // so the map function gets the needed data from the marketData and returns a list of statistics
             .map(mapGlobalMarketData)
         
             .sink { [weak self] returnedStats in
                 self?.statistics = returnedStats
+                self?.isLoading = false
             }
             .store(in: &cancellables)
         
         
         
-        // Updates portfolioCoins
-        $allCoins
-            .combineLatest(portfolioDataService.$savedEntities)
-            .map({ (coinModels, portfolioEntities) -> [CoinModel] in
-                coinModels.compactMap { (coin) -> CoinModel? in
-                    guard let entity = portfolioEntities.first(where: {$0.coinId == coin.id}) else {return nil}
-                    
-                    return coin.updateHoldings(amount: entity.amount)
-                            
-                }
-            })
-//            .map { [weak self] (coinModels, portfolioEntities) -> [CoinModel] in
-//                var portfolioCoins: [CoinModel] = []
-//
-//                // Loop through all the entities in the portfolio
-//                for portfolioEntity in portfolioEntities{
-//                    // Get the coin from all coins that has the same id as the coinId in the entity and add it to the portfolioCoins
-//                    if let coin = coinModels.first(where: {$0.id == portfolioEntity.coinId}){
-//                        portfolioCoins.append(coin)
-//                    }
-//                }
-//
-//                return portfolioCoins
-//            }
-            .sink { [weak self] returnedPortfolioCoins in
-                self?.portfolioCoins = returnedPortfolioCoins
-            }
-            .store(in: &cancellables)
-            
-    
     }
     
     
@@ -101,6 +95,15 @@ class HomeViewModel: ObservableObject{
         portfolioDataService.updatePortfolio(coin: coin, amount: amount)
     }
     
+    
+    
+    func reloadData(){
+        isLoading = true
+        coinDataService.getCoins()
+        marketDataService.getData()
+        HapticManager.notification(type: .success)
+    }
+
     
     private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel]{
         // No text in the searchbar
@@ -118,7 +121,25 @@ class HomeViewModel: ObservableObject{
     }
     
     
-    private func mapGlobalMarketData(marketDataModel: MarketDataModel?) -> [StatisticModel]{
+    private func mapAllCoinsToPortfolioCoins(allCoins: [CoinModel], portfolioEntities: [PortfolioEntity]) -> [CoinModel]{
+        var portfolioCoins: [CoinModel] = []
+
+        // Loop through allx the entities in the portfolio
+        for portfolioEntity in portfolioEntities{
+            // Get the coin from all coins that has the same id as the coinId in the entity and add it to the portfolioCoins
+            if let coin = allCoins.first(where: {$0.id == portfolioEntity.coinId}){
+                portfolioCoins.append(coin.updateHoldings(amount: portfolioEntity.amount))
+            }
+        }
+
+        return portfolioCoins
+    }
+    
+    
+    
+    
+    
+    private func mapGlobalMarketData(marketDataModel: MarketDataModel?, portfolioCoins: [CoinModel]) -> [StatisticModel]{
         var stats: [StatisticModel] = []
         
         guard let data = marketDataModel else {return stats}
@@ -129,7 +150,27 @@ class HomeViewModel: ObservableObject{
         
         let btcDominance = StatisticModel(title: "BTC Dominance", value: data.btcDominance)
         
-        let portfolio = StatisticModel(title: "Portfolio Value", value: "$0.00", percentageChange: 0)
+        
+        let portfolioValue =
+            portfolioCoins
+                .map({$0.currentHoldingsValue})
+                .reduce(0, +)
+        
+        
+        let previousValue =
+            portfolioCoins
+            .map { (coin) -> Double in
+                let currentValue = coin.currentHoldingsValue
+                let percentChange = coin.priceChangePercentage24H ?? 0 / 100
+                let previousValue = currentValue / (1 + percentChange)
+                return previousValue
+            }
+            .reduce(0, +)
+        
+        let percentageChange = ((portfolioValue - previousValue) / previousValue) * 100
+        
+
+        let portfolio = StatisticModel(title: "Portfolio Value", value: portfolioValue.asCurrencyWith2Decimals(), percentageChange: percentageChange)
         
         stats.append(contentsOf: [
             marketCap,
--- SwiftfulCrypto/Core/Home/Views/HomeView.swift
+++ SwiftfulCrypto/Core/Home/Views/HomeView.swift
@@ -119,6 +119,9 @@ extension HomeView{
             }
         }
         .listStyle(.plain)
+        .refreshable {
+            vm.reloadData()
+        }
     }
     
     private var portfolioCoinsList: some View{
@@ -131,6 +134,9 @@ extension HomeView{
             
         }
         .listStyle(.plain)
+        .refreshable {
+            vm.reloadData()
+        }
     }
     
     
@@ -147,6 +153,16 @@ extension HomeView{
             
             Text("Price")
                 .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
+            
+            Button {
+                withAnimation(.linear(duration: 2)){
+                    vm.reloadData()
+                }
+            } label: {
+                Image(systemName: "goforward")
+                    .rotationEffect(Angle(degrees: vm.isLoading ? 360 : 0), anchor: .center)
+            }
+
         }
         .font(.caption)
         .foregroundColor(.theme.secondaryText)
